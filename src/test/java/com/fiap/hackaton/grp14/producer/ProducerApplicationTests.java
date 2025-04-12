911e285644a0b396e1c5ab291a1932c0afe06d0b
Eduardo Ávila <eavilalima@gmail.com>
2025-04-12 09:54:26 -0300

    Rename da classe application do Spring Boot

---
+++ src/test/java/com/fiap/hackaton/grp14/producer/ProducerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.fiap.hackaton.grp14.producer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class ProducerApplicationTests {
+
+	@Test
+	void dummyTest() {
+		assertEquals("123", "123");
+	}
+
+}
